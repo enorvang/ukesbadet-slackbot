0da2e0107d27e8b78e5a4c6f37dff3df5b1b7554
Francis Dyrhovden <soliman_61@hotmail.com>
2022-10-03 23:39:49 +0200

    feat: add slash commands

--- index.js
+++ index.js
@@ -27,55 +27,104 @@ receiver.router.post("/slack/events", (req, res) => {
   if (req?.body?.challenge) res.send({ challenge });
 });
 
-receiver.router.post("/interactive", (req, res) => {
-  console.log(req);
-});
-
-app.command("/badet", async ({ command, ack, respond }) => {
-  // Acknowledge command request
+app.command("/badet", async ({ ack, say, command }) => {
   await ack();
-  console.log(command);
-  await say(`${command.text}`);
+
+  if (!command.text.includes("@")) {
+    await say(
+      `<@${command.user_id}> Du må tagge den du har badet med for å registrere badet`
+    );
+    return;
+  }
+
+  const userId = command.text.split("@")[1].split("|")[0];
+  const { data, error } = await supabaseClient
+    .from("users")
+    .select("*")
+    .eq("slack_username", userId);
+
+  if (data.length === 1) {
+    const badeBuddy = data[0].slack_id;
+    await supabaseClient
+      .from("baths")
+      .insert([
+        { user_slack_id: command.user_id },
+        { user_slack_id: badeBuddy },
+      ]);
+
+    await say(`<@${command.user_id}> har badet med <@${badeBuddy}>!`);
+  }
 });
 
-app.message(`score`, async (data) => {
+app.command(`/score`, async ({ ack, say, command }) => {
+  await ack();
   const { count } = await supabaseClient
     .from("baths")
     .select("*", { count: "exact" })
-    .eq("user_slack_id", data.message.user);
+    .eq("user_slack_id", command.user_id);
 
-  await data.say(`<@${data.message.user}> har badet ${count} ganger`);
+  await say(`<@${command.user_id}> har badet ${count} ganger`);
 });
 
-app.event("member_joined_channel", async (data) => {
-  const { user, channel } = data.event;
+app.command(`/info`, async ({ ack, say, command }) => {
+  await ack();
+  const { channel_id } = command;
+
+  await say({
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `<#${channel_id}> er et engasjement som har fokus på personlig helse og å sosialisere seg med kollegaer i Stacc:star:. Konseptet går ut på å bade hver uke sammen med dine kollegaer.`,
+        },
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "*Reglene er enkle:*\n• Man kan maks få 1 poeng per uke\n• Man er nødt til å bade sammen med én eller flere kollegaer",
+        },
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "Skriv `/help` for å se en liste over kommandoer",
+        },
+      },
+    ],
+  });
+});
+
+app.command(`/help`, async ({ ack, say }) => {
+  await ack();
+  await say({
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "*Kommandoer:*\n• `/info` - Info om konseptet\n• `/register` - Registrerer brukeren din i databasen\n• `/badet @<din-badebuddy>` - Registrerer et bad for deg og den du har badet med. Dette kan kun gjøres én gang per uke\n• `/score` - Viser hvor mange ganger du har badet",
+        },
+      },
+    ],
+  });
+});
+
+app.command(`/register`, async ({ ack, say, command }) => {
+  await ack();
+  const { user_id, user_name, channel_id } = command;
   const { data: users } = await supabaseClient
     .from("users")
     .select("slack_id")
-    .eq("slack_id", user);
+    .eq("slack_id", user_id);
   if (users.length === 0) {
-    await supabaseClient.from("users").insert([{ slack_id: user }]);
+    await supabaseClient
+      .from("users")
+      .insert([{ slack_id: user_id, slack_username: user_name }]);
+    await say(
+      `Hei <@${user_id}> og velkommen til <#${channel_id}>! Skriv /info for å lese mer om konseptet.`
+    );
   }
-  await data.say(
-    `Hei <@${user}>! Velkommen til <#${channel}>! For å få poeng må du skrive badet i kanalen.`
-  );
-});
-
-app.message(`badet`, async (data) => {
-  await supabaseClient
-    .from("baths")
-    .insert([{ user_slack_id: data.message.user }]);
-
-  const { count } = await supabaseClient
-    .from("baths")
-    .select("*", { count: "exact" })
-    .eq("user_slack_id", data.message.user);
-
-  // const test = await supabaseClient
-  //   .rpc("increment", {
-  //     row_id: data.message.user,
-  //   })
-  // console.log(test);
-
-  await data.say(`<@${data.message.user}> har nå badet ${count} ganger`);
 });
